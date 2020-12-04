005e9c11de8a518093ac91c3ba47f75272d3d448
Dmitry Romashov <framebassman@gmail.com>
2020-12-05 01:41:11 +0300

    static

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,12 @@
 using System;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.FileProviders;
 
 namespace Dummy.Web
 {
@@ -25,6 +27,12 @@ namespace Dummy.Web
             }
 
             app.UseRouting();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(env.ContentRootPath, "static")),
+                RequestPath = "/static"
+            });
 
             app.UseEndpoints(endpoints =>
             {
